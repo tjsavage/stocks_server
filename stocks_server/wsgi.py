14324740b40bd0310978a033f7e9ba519fe05e78
Taylor Savage <git@taylorsavage.com>
2011-11-30 17:47:54 -0800

    Still tweaking wsgi.py

--- stocks_server/wsgi.py
+++ stocks_server/wsgi.py
@@ -14,9 +14,7 @@ framework.
 
 """
 import os
-import sys
 
-sys.path.append(os.path.dirname(__file__))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 # This application object is used by any WSGI server configured to use this
